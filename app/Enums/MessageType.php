7bc687bfcc599c1e3a1a10dc46d4930bbc2a2f76
Verga Tandika <verga.tandika@gmail.com>
2024-11-01 00:16:14 +0700

    init commit

---
+++ app/Enums/MessageType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Enums;
+
+enum MessageType: string
+{
+    case CREATED = 'Berhasil menambahkan';
+    case UPDATED = 'Berhasil memperbarui';
+    case DELETED = 'Berhasil menghapus';
+    case ERROR = 'Terjadi kesalahan. Silahkan coba lagi nanti';
+
+    public function message(string $entity, ?string $error = null): string
+    {
+        if($this === MessageType::ERROR && $error){
+            return "{$this->value} {$error}";
+        }
+
+        return "{$this->value} {$entity}";
+    }
+}
